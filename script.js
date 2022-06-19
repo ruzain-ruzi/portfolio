9a98d3615ba8e61feec31fe1739008f37e5d0a9d
Ruzain5ruzi <37952257+Ruzain5ruzi@users.noreply.github.com>
2022-06-19 22:24:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -73,3 +73,35 @@ $(document).ready(function(){
 
 
 });
+var form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          status.classList.add('submit')  
+          status.innerHTML = "Submitted";
+          form.reset()
+        } else {
+          response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } else {
+              status.classList.add('error')  
+              status.innerHTML = "Error Occurred"
+            }
+          })
+        }
+      }).catch(error => {
+        status.innerHTML = "Error Occurred"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
